7b76cec22a5013961acd9a05c9217111a5639e9f
Inge Pedersen <inge.pedersen.1980@gmail.com>
2024-10-01 05:17:39 +0200

    gh-page fixes

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
+    basePath: '/drag-n-drop-desktop',
+    trailingSlash: true, // Optional, adds trailing slashes to URLs
 };
 
 export default nextConfig;
